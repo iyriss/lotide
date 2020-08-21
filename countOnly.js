4362d6b110a29ad06dbed7a054f61a0f7edaa9df
Iyris Vigil <iyrisvigil@gmail.com>
2020-08-21 07:12:34 +0000

    refactored and eslinted

--- countOnly.js
+++ countOnly.js
@@ -7,18 +7,22 @@ const assertEqual = function(actual, expected) {
 };
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
-const countOnly = function (allItems, itemsToCount){
-  const results = {}
+//The function should report back how many instances of each string
+  //were found in the allItems array of strings.
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
   for (const item of allItems) {
     if (itemsToCount[item]) { //checking if the names of the first names list exists in the object
-      if (results[item]) { 
-        results[item] += 1;
+      if (results[item]) {
+        results[item] += 1; //adds to the empty object
       } else {
         results[item] = 1;
+      }
     }
   }
   return results;
-}
+};
 
 const firstNames = [
   "Karl",
