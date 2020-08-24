5869c056b9a5c750dc6f46b8835d19fa82ed0478
Iyris Vigil <iyrisvigil@gmail.com>
2020-08-24 20:54:12 +0000

    refactoring middle file and adding middleTest

--- middle.js
+++ middle.js
@@ -1,25 +1,8 @@
-const eqArrays = function(firstArr, secondArr) {
-  for (let i = 0; i < firstArr.length; i++) {
-    if (firstArr[i] !== secondArr[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(expectedArray, resultArray) {
-  if (eqArrays(expectedArray, resultArray)) {
-    console.log(`Passed: ${expectedArray} === ${resultArray}`);
-  } else {
-    console.log(`Failed: ${expectedArray} !== ${resultArray}`);
-  }
-};
-
 const middle = function(array) {
   let arrLength = array.length;
   let midNum = [];
-  if (arrLength === 1 || arrLength === 2) {
-    return midNum;
+  if (arrLength <= 2) {
+    return [];
   } else {
     let firstMiddle = array.indexOf(Math.ceil(arrLength / 2));
     if (arrLength % 2 === 0) {
@@ -30,10 +13,5 @@ const middle = function(array) {
     } //return midNum;
   } return midNum;
 };
- 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1]),[[]]);
-assertArraysEqual(middle([1, 2]), [[]]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]);
-assertArraysEqual(middle([1, 2, 3, 4]),[2,3]);
+
+module.exports = middle;
